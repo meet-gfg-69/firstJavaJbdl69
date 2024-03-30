eececc017d25fe037406f86481aa3aea6443442e
meetd <bikash081995@gmail.com>
2024-03-30 18:12:27 +0530

    class 3 commit.

--- src/main/java/org/example/service/MyFunctionalInterface.java
+++ src/main/java/org/example/service/MyFunctionalInterface.java
@@ -20,7 +20,7 @@ public interface MyFunctionalInterface {
 
 /***
  *
- *  producer  -> which will not accept anything, but will give the data  ->  instagram or content feed.
+ *  supplier  -> which will not accept anything, but will give the data  ->  instagram or content feed.
  *  consumer  -> which will accept the parameter and will not return anything -> data analytics, form submit
  *  predicate  -> which will accept the parameter and will return any parameter ->
  *
